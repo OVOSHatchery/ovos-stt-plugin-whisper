4bcc651e197522d27f52cc82ae99877091ade902
jarbasai <jarbasai@mailfence.com>
2023-01-25 18:34:59 +0000

    model selection

--- ovos_stt_plugin_whisper/__init__.py
+++ ovos_stt_plugin_whisper/__init__.py
@@ -1,17 +1,27 @@
 import speech_recognition as sr
 from ovos_plugin_manager.templates.stt import STT
 from whisper.tokenizer import LANGUAGES
+import whisper
 
 
 class WhisperSTT(STT):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.model = self.config.get("model", "base")
         self.recognizer = sr.Recognizer()
+        self.load_model()
+
+    def load_model(self):
+        # will auto download if missing
+        if not hasattr(self.recognizer, "whisper_model"):
+            self.recognizer.whisper_model = {}
+        self.recognizer.whisper_model[self.model] = whisper.load_model(self.model)
 
     def execute(self, audio, language=None):
         lang = language or self.lang
         lang = lang.split("-")[0]
-        utt = self.recognizer.recognize_whisper(audio, language=lang)
+        utt = self.recognizer.recognize_whisper(audio, language=lang,
+                                                model=self.model)
         return utt
 
     def available_languages(self) -> set:
--- readme.md
+++ readme.md
@@ -13,7 +13,8 @@ By default the global language used by mycroft-core will be used
 
 ```json
   "stt": {
-    "module": "ovos-stt-plugin-whisper"
+    "module": "ovos-stt-plugin-whisper",
+    "ovos-stt-plugin-whisper": {"model": "base"}
   }
  
 ```
